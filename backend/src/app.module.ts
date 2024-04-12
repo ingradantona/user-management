587954306bd5423abf0f43c8806353c4fadc5ed1
Ingra De Souza D'Antona <ingradantona.arq@gmail.com>
2024-04-12 18:18:53 -0400

    feat: init user resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SwaggerModule } from './config/swagger/swagger.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './config/database/typeorm.config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(AppDataSource.options), SwaggerModule],
+  imports: [TypeOrmModule.forRoot(AppDataSource.options), SwaggerModule, UserModule],
   providers: [],
 })
 export class AppModule {}
