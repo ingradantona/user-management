9ee17cf120f89da8cdd42311a4d6e58e90f958f0
Ingra De Souza D'Antona <ingradantona.arq@gmail.com>
2024-04-13 08:45:05 -0400

    feat: create user dto

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty()
+  user_name: string;
+
+  @ApiProperty()
+  user_surname: string;
+
+  @ApiProperty({ example: 'example@example.com' })
+  user_email: string;
+
+  @ApiProperty()
+  user_password: string;
+}
