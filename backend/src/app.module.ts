4eb84bb26e8ff3e60c4a5081197918e41c806f2c
Ingra De Souza D'Antona <ingradantona.arq@gmail.com>
2024-04-12 21:55:33 -0400

    feat: create user table migration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SwaggerModule } from './config/swagger/swagger.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppDataSource } from './config/database/typeorm.config';
+import { AppDataSource } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 
 @Module({
--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column({ nullable: false, type: 'varchar' })
+  user_name: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  user_surname: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  user_email: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  user_password: string;
+
+  @Column({
+    type: 'bit',
+  })
+  user_status: boolean;
+}
